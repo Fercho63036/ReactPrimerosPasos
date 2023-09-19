70b719d74bf397e2d0cf2194d1b3a2d335a6ebb6
Ariel Paricagua <ArielFParicaguaV@gmail.com>
2023-09-19 17:09:05 -0400

    Primer commit

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -11,14 +11,22 @@ export const CounterApp = ({ value }) => {
         setCounter( (c) => c + 1 );
     }
 
+    const handleRemove = () => {
+        setCounter( (c) => c - 1);
+    }
+
+    const handleReset = () => {
+        setCounter( value );
+    }
+
     return (
         <>
             <h1> CounterApp </h1>
             <h2> { counter } </h2>
 
-            <button onClick={ handleAdd }>
-                +1
-            </button>
+            <button onClick={ handleAdd }> +1 </button>
+            <button onClick={ handleRemove }> -1 </button>
+            <button onClick={ handleReset }> Reset </button>
         </>
     )
 }
